5025c3852b4ae60248d4643873cec4bf5298d829
Ava Hill <hillma54@gateway-01.dmz>
2024-05-16 10:33:27 -0400

    Cleaned up the repository and removed some unneeded files.

--- sn112/doMyJob2.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-./chisq_5_7.bash
-./chisq_5_9.bash
--- sn116/run_gosia2.sh
+++
@@ -1,7 +0,0 @@
-#~/bin/bash
-
-gosia2_sn116 < sn116_gos2.INTI.inp
-gosia2_sn116 < pt196_gos2.INTI.inp
-gosia < sn116_gos2.MAP.inp
-gosia < pt196_gos2.MAP.inp
-gosia2_sn116 < sn116_gos2.inp
